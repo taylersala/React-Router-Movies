6e8d207e29c488cfc23570ec3c31e8ed7bbae498
taylersala <taylersala@gmail.com>
2023-05-25 12:47:43 -0700

    finished

--- src/Movies/MovieList.js
+++ src/Movies/MovieList.js
@@ -1,20 +1,27 @@
 import React from 'react';
+import { useNavigate, Link } from 'react-router-dom'
 
 export default function MovieList(props) {
+  const navigate = useNavigate()
+  const onMovieClick = id => () => {
+    navigate(`movies/${id}`)
+  }
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+        <MovieDetails link={<Link to={`movies/${movie.id}`}>Details</Link>} onMovieClick={onMovieClick(movie.id)} key={movie.id} movie={movie} />
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
+
   const { title, director, metascore } = props.movie;
+  const { onMovieClick, link } = props;
 
   return (
-    <div className="movie-card">
+    <div className="movie-card" onClick={onMovieClick}>
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -22,6 +29,7 @@ function MovieDetails(props) {
       <div className="movie-metascore">
         Metascore: <strong>{metascore}</strong>
       </div>
+      {link}
     </div>
   );
 }
